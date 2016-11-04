a9676a8d10a4e145711d39c5043c920c4f94b0b2
Patrick Trinkle <patrick@activatr.com>
2016-11-04 13:18:59 -0400

    moved the compose file up, and removed extra code from the view.

--- api/image_app/views/image/__init__.py
+++ api/image_app/views/image/__init__.py
@@ -17,27 +17,10 @@ class ImageViewSet(viewsets.ModelViewSet):
     queryset = Image.objects.all()
     serializer_class = ImageSerializer
 
-    def create(self, request, **kwargs):
-        """
-        Create but return 201 on success.
-        """
+    def get_queryset(self):
 
-        serializer_class = self.get_serializer_class()
-
-        serializer = serializer_class(data=request.data, context={'request': request})
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def list(self, request, **kwargs):
-        """
-        List the Images.
-        """
-
-        labels = request.query_params.getlist('labels', [])
-        unlabeled = request.query_params.get('unlabeled', False)
+        labels = self.request.query_params.getlist('labels', [])
+        unlabeled = self.request.query_params.get('unlabeled', False)
 
         import sys
         from json import dumps
@@ -52,7 +35,4 @@ class ImageViewSet(viewsets.ModelViewSet):
             else:
                 queryset = Image.objects.all()
 
-        serializer = ImageSerializer(queryset, many=True, context={'request': request})
-
-        return Response(serializer.data)
-
+        return queryset
